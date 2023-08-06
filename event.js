ad24e43141c546a4680a312a21c26a22ea9bcb48
Federico Trotta <ftrotta18@gmail.com>
2023-08-06 16:10:15 -0300

    solucion boton

--- event.js
+++ event.js
@@ -1,7 +1,19 @@
 const div = document.getElementById("myDiv");
-div.addEventListener("click", function asd () {
-  alert("Hola! Soy el div");
+const button = document.getElementById("button");
+
+button.addEventListener("mouseover", () => {
+  div.removeEventListener("click", handleClickDiv);
 });
+
+button.addEventListener("mouseout", () => {
+  div.addEventListener("click", handleClickDiv);
+});
+
+div.addEventListener("click", handleClickDiv);
+
+function handleClickDiv() {
+  alert("Hola! Soy el div");
+}
 
 function handleClick() {
   alert("Hola!");
